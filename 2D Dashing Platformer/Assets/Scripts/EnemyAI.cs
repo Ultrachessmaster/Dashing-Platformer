bbf847e54e260f9879625383d717f98b186ecb16
Ultrachessmaster <oldrogerisme@gmail.com>
2015-03-30 22:49:13 -0600

    Alpha 1.1.2 Fixed slime AI so it works perfectly, with some hiccups in movement. Tried to fix subpixel movement of slimes, but they started moving weirdly when I did. Also changed the scale a bit of the slimes to fit the level (again).

--- 2D Dashing Platformer/Assets/Scripts/EnemyAI.cs
+++ 2D Dashing Platformer/Assets/Scripts/EnemyAI.cs
@@ -9,27 +9,29 @@ public class EnemyAI : MonoBehaviour {
 	private Rigidbody2D rb2D;
 	private bool right;
 	private bool onscreen = false;
+	private bool closetoedge = false;
 
 	// Use this for initialization
 	void Start () {
 		rb2D = GetComponent<Rigidbody2D> ();
 	}
-	
-	// Update is called once per frame
+
+	void Update () {
+
+	}
+
 	void FixedUpdate () {
 		if(onscreen) {
 			Vector2 newPosition = transform.position;
-			float x = newPosition.x;
-			if(!right) {
-				x -= speed;
-				right = false;
-			}
-			else {
-				x += speed;
-				right = true;
-			}
-			newPosition.x = x;
+			newPosition.x += (right ? speed : -speed);
 			rb2D.MovePosition (newPosition);
+		}
+		//Don't do Physics2D.Racast until slime is no longer near edge. Other wise it will never leave as it will be constantly switching back and forth.
+		if(!Physics2D.Raycast (new Vector2(transform.position.x + 0.16f, transform.position.y), -Vector2.up, 0.48f)) {
+			right = false;
+		}
+		if(!Physics2D.Raycast (new Vector2(transform.position.x - 0.16f, transform.position.y), -Vector2.up, 0.48f)) {
+			right = true;
 		}
 	}
 
@@ -38,28 +40,25 @@ public class EnemyAI : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D (Collision2D col) {
-		if(col.collider.CompareTag ("Player") &
-			GameObject.FindGameObjectWithTag ("Player").GetComponent <Rigidbody2D> ().velocity.y >= 0) {
-
-			Debug.Log ("Player falling" + 
-				GameObject.FindGameObjectWithTag ("Player").GetComponent <Rigidbody2D> ().velocity.y);
+		if(!col.collider.CompareTag ("Player"))
+			return;
+		Vector2 playervelocity = GameObject.FindGameObjectWithTag ("Player").GetComponent <Rigidbody2D> ().velocity;
+		if(playervelocity.y >= 0) {
 			Application.LoadLevel (Application.loadedLevel);
-
 		}
-		else if (col.collider.CompareTag ("Player") & GameObject.FindGameObjectWithTag ("Player").GetComponent <Rigidbody2D> ().velocity.y < 0) {
+		else {
 			gameObject.SetActive (false);
-			Vector2 playerVelocity = GameObject.FindGameObjectWithTag ("Player").GetComponent <Rigidbody2D> ().velocity;
-			playerVelocity.y = bounciness;
-			GameObject.FindGameObjectWithTag ("Player").GetComponent <Rigidbody2D> ().velocity = playerVelocity;
+			playervelocity.y = bounciness;
+			GameObject.FindGameObjectWithTag ("Player").GetComponent <Rigidbody2D> ().velocity = playervelocity;
 
 		}
 	}
 
 	void LateUpdate () {
 		Vector2 newPosition = transform.position;
-		newPosition *= 100f;
+		newPosition *= 50f;
 		newPosition = new Vector2 (Mathf.Round (newPosition.x), Mathf.Round (newPosition.y));
-		newPosition *= (1/100f);
+		newPosition *= (1/50f);
 		transform.position = newPosition;
 	}
 
